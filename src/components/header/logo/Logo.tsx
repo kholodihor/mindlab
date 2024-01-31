a16b0e1fc767a8887349cfd9cd5669c35ce8ad6a
cold <kholodihor@gmail.com>
2024-01-31 08:18:49 +0200

    feat: update teacher page and logo

--- src/components/header/logo/Logo.tsx
+++ src/components/header/logo/Logo.tsx
@@ -1,10 +1,12 @@
 'use client'
 
 import { useState } from 'react'
-import { motion } from 'framer-motion'
 import Link from 'next/link'
+import Lottie from 'lottie-react'
+import logo_open from '@/animations/logo_open.json'
+import logo_close from '@/animations/logo_close.json'
 import styles from './Logo.module.css'
-import TypingText from '@/components/TypingText'
+
 const Logo = () => {
   const [isHovered, setIsHovered] = useState(false)
 
@@ -14,19 +16,9 @@ const Logo = () => {
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      {!isHovered ? (
-        <Link href="/">
-          <h1>ML</h1>
-        </Link>
-      ) : (
-        <Link href="/">
-          <motion.div initial="hidden" whileHover="visible" viewport={{ amount: 0.1 }}>
-            <h1 className={styles.title}>
-              <TypingText title="MIND LAB" />
-            </h1>
-          </motion.div>
-        </Link>
-      )}
+      <Link href="/">
+        <Lottie animationData={isHovered ? logo_open : logo_close} loop={false} />
+      </Link>
     </div>
   )
 }
