123cf11d7ea88387309019906648674216ae7f19
cold <kholodihor@gmail.com>
2024-02-13 10:10:31 +0200

    feat(hero): add adaptation

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,60 +1,16 @@
 'use client'
 
-import Image from 'next/image'
-import { useMediaQuery } from '@react-hook/media-query'
-import Lottie from 'lottie-react'
-import hero from '@/animations/hero.json'
-import RotatingStar from '../shared/rotating_star/RotatingStar'
-
+import TitleBlock from './title_block/TitleBlock'
+import AnimationBlock from './animation_block/AnimationBlock'
 import styles from './Hero.module.css'
-import HeroMobile from './hero_mobile/HeroMobile'
-import HeroTablet from './hero_tablet/HeroTablet'
 
 const Hero = () => {
-  const isDesktop = useMediaQuery('(min-width: 1280px)')
-  const isTablet = useMediaQuery('(max-width: 1279px)')
-  const isMobile = useMediaQuery('(max-width: 768px)')
-
   return (
-    <>
-      {isDesktop && (
-        <div className={`${styles.hero} container`}>
-          <div className={styles.title_block}>
-            <h1 className={styles.title_block_title}>
-              Платформа <br /> твого <br />
-              безмежного
-              <br />
-              розвитку
-            </h1>
-            <p className={styles.title_block_paragraph}>
-              Кайфуй від того, що приносить тобі користь
-            </p>
-            <Image
-              width={200}
-              height={200}
-              className={styles.star_green}
-              src="/svg/hero/star_green.svg"
-              alt=""
-            />
-            <Image
-              width={120}
-              height={120}
-              className={styles.star_violet}
-              src="/svg/hero/star_violet.svg"
-              alt=""
-            />
-          </div>
-          <div className={styles.rotating_star}>
-            <RotatingStar />
-          </div>
-          <div className={styles.animation_block}>
-            <Lottie animationData={hero} loop={false} />
-          </div>
-        </div>
-      )}
-      {isTablet && <HeroTablet />}
-      {isMobile && <HeroMobile />}
-    </>
+    <section className={`${styles.hero} container`}>
+      <TitleBlock />
+      <AnimationBlock />
+      <p className={styles.paragraph}>Кайфуй від того, що приносить тобі користь</p>
+    </section>
   )
 }
 
--- src/components/teachers/slider/Slider.tsx
+++ src/components/teachers/slider/Slider.tsx
@@ -1,11 +1,12 @@
 'use client'
 
-import React, { useRef } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Navigation } from 'swiper/modules'
 import { teachers } from '@/data/teachers'
+import { useMediaQuery } from '@react-hook/media-query'
 import TeacherCard from '@/components/shared/teacher_card/TeacherCard'
 
 import styles from './Slider.module.css'
@@ -14,6 +15,30 @@ import 'swiper/css'
 
 const Slider = () => {
   const sliderRef = useRef(null)
+  const [amount, setAmount] = useState(0)
+
+  const isLargeScreen = useMediaQuery('(min-width: 1281px)')
+  const isMediumScreen = useMediaQuery('(max-width: 1280px)')
+  const isSmallScreen = useMediaQuery('(max-width: 991px)')
+  const isExtraSmallScreen = useMediaQuery('(max-width: 678px)')
+
+  useEffect(() => {
+    const getAmountOfSlides = () => {
+      if (isMediumScreen) {
+        setAmount(3)
+      }
+      if (isSmallScreen) {
+        setAmount(2)
+      }
+      if (isLargeScreen) {
+        setAmount(4)
+      }
+      if (isExtraSmallScreen) {
+        setAmount(1)
+      }
+    }
+    getAmountOfSlides()
+  }, [isMediumScreen, isSmallScreen, isLargeScreen, isExtraSmallScreen])
 
   const handlePrev = () => {
     if (sliderRef && sliderRef.current) {
@@ -34,7 +59,7 @@ const Slider = () => {
       <div className={styles.swiper}>
         <Swiper
           spaceBetween={10}
-          slidesPerView={4}
+          slidesPerView={amount}
           loop={true}
           modules={[Navigation]}
           onSwiper={(swiper) => {
