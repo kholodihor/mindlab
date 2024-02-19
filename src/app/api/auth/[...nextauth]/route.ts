b222f869c819159824f039e87b04fc39d2f87f84
cold <kholodihor@gmail.com>
2024-02-19 15:59:33 +0200

    fix(nextauth): fix exports

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,59 +1,6 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-import NextAuth, { AuthOptions } from 'next-auth'
-import CredentialsProvider from 'next-auth/providers/credentials'
-import bcrypt from 'bcrypt'
-import prisma from '@/lib/prisma'
-
-export const authOptions: AuthOptions = {
-  providers: [
-    CredentialsProvider({
-      id: 'credentials',
-      name: 'Credentials',
-      credentials: {
-        email: {
-          label: 'email',
-          type: 'text'
-        },
-        password: {
-          label: 'password',
-          type: 'password'
-        }
-      },
-      // @ts-ignore
-      async authorize(credentials: { email: string; password: string }) {
-        if (!credentials.email || !credentials.password) {
-          throw new Error('Invalid email or password')
-        }
-
-        const user = await prisma.user.findUnique({
-          where: {
-            email: credentials.email
-          }
-        })
-
-        if (!user || !user?.hashedPassword) {
-          throw new Error('User not found')
-        }
-
-        const isCorrectPassword = await bcrypt.compare(credentials.password, user.hashedPassword)
-
-        if (!isCorrectPassword) {
-          throw new Error('Invalid email or password')
-        }
-
-        return user
-      }
-    })
-  ],
-  pages: {
-    signIn: '/login'
-  },
-  debug: process.env.NODE_ENV === 'development',
-  session: {
-    strategy: 'jwt'
-  },
-  secret: process.env.NEXTAUTH_SECRET
-}
+import { authOptions } from '@/utils/authOptions'
+import NextAuth from 'next-auth'
 
 const handler = NextAuth(authOptions)
 
