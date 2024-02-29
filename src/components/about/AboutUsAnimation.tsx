ef8d9988350a2b73bae13c507b3156f4f81be476
sofiiaruban <sophiaruban249324@gmail.com>
2024-02-29 17:56:54 +0200

    feature: Add eng version to about desktop

--- src/components/about/AboutUsAnimation.tsx
+++ src/components/about/AboutUsAnimation.tsx
@@ -2,7 +2,9 @@
 
 import React, { FC } from 'react'
 import Lottie from 'lottie-react'
-import aboutUs from '@/animations/about_us.json'
+import aboutUsUa from '@/animations/about_us.json'
+import aboutUsEng from '@/animations/about_us_eng.json'
+import { useLocale } from 'next-intl'
 import { useWindowScrollY } from './useWindowScrollY'
 
 interface AboutUsAnimationProps {
@@ -14,16 +16,27 @@ const style = {
   height: '120%',
   marginLeft: '-118px'
 }
+const styleEn = {
+  width: '120%',
+  height: '120%'
+}
 const divisionFactor = 4
 
 const AboutUsAnimation: FC<AboutUsAnimationProps> = ({ aboutRef }) => {
   const { scrollY } = useWindowScrollY()
   const aboutPosition = aboutRef.current
   const startPlayPosition = (aboutPosition?.offsetTop ?? 0) / divisionFactor
+  const DEFAULT_LANGUAGE = 'ua'
+  const currentLanguage = useLocale()
+
   return (
     <>
       {scrollY >= startPlayPosition && (
-        <Lottie animationData={aboutUs} style={style} loop={false} />
+        <Lottie
+          animationData={currentLanguage === DEFAULT_LANGUAGE ? aboutUsUa : aboutUsEng}
+          style={currentLanguage === DEFAULT_LANGUAGE ? style : styleEn}
+          loop={false}
+        />
       )}
     </>
   )
