e263705e5784e8157487f1c5cfa4f60c6c158709
Olena Surilova <elen21surilova@gmail.com>
2024-04-04 13:47:35 +0300

    fix: add use client ditective

--- src/components/admin/shared/pageTitle/PageTitle.tsx
+++ src/components/admin/shared/pageTitle/PageTitle.tsx
@@ -1,3 +1,4 @@
+"use client";
 import styles from './PageTitle.module.css'
 import AddButton from '../addButton/AddButton';
 import PasswordButton from '../settingsButton/PasswordButton';
@@ -13,9 +14,9 @@ const PageTitle = ({ title, isAddButtonDisplayed, isSettingsButtonDisplayed }: P
     <div className={styles.container}>
       <h2 className={styles.title}>{title}</h2>
       <div className={styles.button_container}>
-        {isAddButtonDisplayed && <AddButton text={"text text"} handleClick={() => {}}/>}
+        {isAddButtonDisplayed && <AddButton text={"text text"} handleClick={() => { }} />}
 
-        {isSettingsButtonDisplayed && <PasswordButton handleClick={() => {}} />}
+        {isSettingsButtonDisplayed && <PasswordButton handleClick={() => { }} />}
       </div>
     </div>
   )
