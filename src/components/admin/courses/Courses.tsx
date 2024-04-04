4e5a4c0423ebe39b66da6b008e6f6bbb945422a9
Olena Surilova <elen21surilova@gmail.com>
2024-04-04 13:40:21 +0300

    feat: markup admin-courses-main page

--- src/components/admin/courses/Courses.tsx
+++ src/components/admin/courses/Courses.tsx
@@ -1,8 +1,39 @@
 import React from 'react'
 import styles from './Courses.module.css'
+import { coursesList } from '@/data/data'
+import { useTranslations } from 'next-intl'
+import PageTitle from '../shared/pageTitle/PageTitle'
 
 const Courses = () => {
-  return <div className={styles.wrapper}>Courses page</div>
+  const t = useTranslations("Courses")
+  return <>
+    <PageTitle title='курси' isAddButtonDisplayed={true} isSettingsButtonDisplayed={true} />
+
+    <main className={styles.main_content}>
+      <div className={styles.wrapper}>
+        <ul className={styles.courses}>
+          {coursesList.map(
+            ({ title, description }) => (
+              <li className={styles.courses_card} key={title}>
+                <div className={styles.course_content}>
+                  <h3 className={styles.course_title}>{title}</h3>
+                  <p className={styles.courses_description}>{t(description)}</p>
+                </div>
+                <div className={styles.button_block}>
+                  <button type='button' className={styles.button_edit}>
+                    x
+                  </button>
+                  <button type='button' className={styles.button_delete}>
+                    x
+                  </button>
+                </div>
+              </li>
+            )
+          )}
+        </ul>
+      </div>
+    </main>
+  </>
 }
 
 export default Courses
