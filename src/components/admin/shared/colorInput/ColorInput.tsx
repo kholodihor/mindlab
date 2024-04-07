67685be296218aad3d032dffb4edd16359c8cc3e
mariastetciuk <114301391+mariastetciuk@users.noreply.github.com>
2024-04-07 19:06:13 +0300

    add component

--- src/components/admin/shared/colorInput/ColorInput.tsx
+++ src/components/admin/shared/colorInput/ColorInput.tsx
@@ -1,29 +1,42 @@
 'use client'
-
-import { InputHTMLAttributes } from 'react'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { InputHTMLAttributes, ForwardedRef, forwardRef  } from 'react'
 import css from './ColorInput.module.css'
 import Image from 'next/image'
 
 interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {
-  color: string
+  colorList: Array<string>
+  title: string
   errorText?: string | any
 }
 
-const ColorInput = ({ color, errorText, ...rest }: TextInputProps) => {
+const ColorInput = forwardRef(function TextInput(
+  { title, errorText, colorList, ...rest }: TextInputProps,
+  _ref: ForwardedRef<HTMLInputElement>
+) {
+
   return (
-    <label className={css.label}>
-      <input type="radio" {...rest} value={color} className={css.input} />
-      <span className={css.span} style={{ backgroundColor: color }}></span>
-      {errorText && errorText !== undefined && <span className={css.error}>{errorText}</span>}
-      <Image
-        src={'/svg/admin/checkmark.svg'}
-        width={14}
-        height={14}
-        alt="icon"
-        className={css.icon}
-      />
-    </label>
-  )
-}
+   <div className={css.wrapper__color}>
+    <h3 className={css.title__color}>{title}</h3>
+        {colorList.map((color) => (
+          <label className={css.label} key={color}>
+            <input type="radio" {...rest} value={color} className={css.input} name='color'/>
+            <span className={css.span} style={{ backgroundColor: color }}></span>
+            <Image
+              src={'/svg/admin/checkmark.svg'}
+              width={14}
+             height={14}
+             alt="icon"
+              className={css.icon}
+            />
+         </label>
+        ))}
+         {errorText && errorText !== undefined && <span className={css.error}>{errorText}</span>}
+   </div>
+    )
+
+})
+
 
 export default ColorInput
+
