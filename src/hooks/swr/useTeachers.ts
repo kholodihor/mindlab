0abfa7482ab49adbc685002fcab0e286bc85fd0e
cold <kholodihor@gmail.com>
2024-04-09 08:29:22 +0300

    fix: fix text input

--- src/hooks/swr/useTeachers.ts
+++ src/hooks/swr/useTeachers.ts
@@ -10,10 +10,15 @@ export const useTeachers = () => {
     {}
   )
 
-  const addTeacher = async (item: ITeacher) => {
+  const getTeacherById = (id: string) => {
+    if (Array.isArray(data) && data.length) {
+      return data?.find((item: ITeacherResponse) => item.id === id)
+    }
+  }
+
+  const addTeacher = async (file: File | Blob, item: ITeacher) => {
     try {
-      const newTeacher = await teachersApi.createTeacher(item)
-      console.log(newTeacher)
+      const newTeacher = await teachersApi.createTeacher(file, item)
       if (newTeacher && data) {
         mutate([newTeacher, ...data])
       }
@@ -22,10 +27,34 @@ export const useTeachers = () => {
     }
   }
 
+  const updateTeacher = async (courseId: string, item: ITeacher, file?: File | Blob) => {
+    try {
+      const updatedTeacher = await teachersApi.updateTeacher(courseId, item, file)
+      if (updatedTeacher && data) {
+        mutate()
+      }
+    } catch (error) {
+      throw Promise.reject()
+    }
+  }
+
+  const deleteTeacher = async (id: string) => {
+    try {
+      await teachersApi.deleteTeacher(id)
+      const updatedTeachers = data?.filter((item) => item.id !== id)
+      mutate(updatedTeachers)
+    } catch (error) {
+      throw Promise.reject()
+    }
+  }
+
   return {
     teachers: data,
+    getTeacherById,
     isLoading,
     isError: error,
-    addTeacher
+    addTeacher,
+    updateTeacher,
+    deleteTeacher
   }
 }
--- src/utils/api/teachers.ts
+++ src/utils/api/teachers.ts
@@ -1,4 +1,5 @@
 import axios from '@/config/axios'
+import { UploadResponse } from '@/types'
 import { ITeacher, ITeacherResponse } from '@/types/teachers'
 
 export const getTeachers = async () => {
@@ -10,11 +11,50 @@ export const getTeachers = async () => {
   }
 }
 
-export const createTeacher = async (teacher: ITeacher) => {
+export const getTeacherById = async (id: string) => {
   try {
-    const response = await axios.post<ITeacherResponse>('/teachers', teacher, {
+    const response = await axios.get(`/teachers/${id}`)
+    return response.data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const createTeacher = async (file: File | Blob, teacherData: ITeacher) => {
+  try {
+    const res: UploadResponse = await axios.post('/upload', file)
+    const newTeacher = { ...teacherData, imageId: res.imageId, imageUrl: res.imageUrl }
+    const response = await axios.post<ITeacherResponse>('/teachers', newTeacher, {
       headers: { 'Content-Type': 'application/json' }
     })
+    return response.data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const updateTeacher = async (id: string, teacherData: ITeacher, file?: File | Blob) => {
+  try {
+    if (!file) {
+      const response = await axios.patch<ITeacherResponse>(`/teachers/${id}`, teacherData, {
+        headers: { 'Content-Type': 'application/json' }
+      })
+      return response.data
+    }
+    const res: UploadResponse = await axios.post('/upload', file)
+    const updatedTeacher = { ...teacherData, imageId: res.imageId, imageUrl: res.imageUrl }
+    const response = await axios.patch<ITeacherResponse>(`/teachers/${id}`, updatedTeacher, {
+      headers: { 'Content-Type': 'application/json' }
+    })
+    return response.data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const deleteTeacher = async (id: string) => {
+  try {
+    const response = await axios.delete(`/teachers/${id}`)
     return response.data
   } catch (error) {
     console.log(error)
