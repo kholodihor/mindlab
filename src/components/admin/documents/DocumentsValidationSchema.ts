e58e2e849d37d4e334d5c8df5f10f9646a73a8bd
cold <kholodihor@gmail.com>
2024-05-18 12:31:09 +0300

    fix(admin): fix bugs with validation

--- src/components/admin/documents/DocumentsValidationSchema.ts
+++ src/components/admin/documents/DocumentsValidationSchema.ts
@@ -3,23 +3,21 @@ import { formatBytes } from '@/helpers/formatBytes'
 
 const MAX_FILE_SIZE = 1024 * 1024 * 5
 
-const ACCEPTED_FILE_TYPES = [
-  'application/pdf',
-  'for-url'
-]
+const ACCEPTED_FILE_TYPES = ['application/pdf', 'for-url']
 
 export const DocumentsFormValidation = z.object({
-  fileName: z.string({ required_error: 'Поле повинно бути заповнене'})
-  .trim()
-  .min(4, { message: "Мінімум 4 символи" })
-  .max(60, { message: "Максимум 60 символів" })
-  .regex(/^[A-Za-z\s]*$/, { message: "Поле повинно містити тільки літери" }),
+  fileName: z
+    .string({ required_error: 'Поле повинно бути заповнене' })
+    .trim()
+    .min(4, { message: 'Мінімум 4 символи' })
+    .max(60, { message: 'Максимум 60 символів' })
+    .regex(/^[A-Za-zа-яА-ЯҐґЄєІіЇї\s]*$/, { message: 'Поле повинно містити тільки літери' }),
   file: z
     .any()
     .refine((value) => value?.length > 0, 'Додайте файл')
     .refine((value) => {
-      value && value?.[0]?.size === 0 && value?.[0]?.type === 'for-url';
-      return true;
+      value && value?.[0]?.size === 0 && value?.[0]?.type === 'for-url'
+      return true
     })
     .refine(
       (value) => value?.[0]?.size <= MAX_FILE_SIZE,
@@ -28,7 +26,7 @@ export const DocumentsFormValidation = z.object({
     .refine(
       (value) => ACCEPTED_FILE_TYPES.includes(value?.[0]?.type),
       'Оберіть файл в форматі .pdf.'
-    ),
-});
+    )
+})
 
-export type FormFields = z.infer<typeof DocumentsFormValidation>;
+export type FormFields = z.infer<typeof DocumentsFormValidation>
